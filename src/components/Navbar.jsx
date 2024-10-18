1ed52a89528f9e70ed97c982961a38b5eadd025e
ganzander <ganeshmangla2003@gmail.com>
2024-10-19 03:28:09 +0530

    Navbar Finalized

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,35 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import jwt from "jsonwebtoken";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const [decoded, setDecoded] = useState(null);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const token = localStorage.getItem("AuthToken");
+    if (token) {
+      try {
+        const decodedToken = jwt.decode(JSON.parse(token));
+        setDecoded(decodedToken);
+      } catch (error) {
+        console.error("Failed to decode token", error);
+      }
+    }
+  }, [pathname]);
+
+  function handleLogout() {
+    localStorage.removeItem("AuthToken");
+    location.reload();
+  }
+
+  function toggleDropdown() {
+    setDropdownVisible((prev) => !prev);
+  }
+
   return (
     <div className="relative w-full flex items-center justify-center">
       <div className="custom-navbar flex items-center justify-between w-full">
@@ -14,9 +41,36 @@ export default function Navbar() {
         </div>
 
         <div className="nav-right text-white flex sm:gap-8  text-center text-[17px] sm:text-[25px] ">
-          <Link href="/login" className="font-normal sm:font-medium">
-            Login
-          </Link>
+          {decoded ? (
+            <div className="relative">
+              <div
+                className="font-normal sm:font-medium cursor-pointer"
+                onClick={toggleDropdown}
+              >
+                {`Hi, ${decoded.fname}`}
+              </div>
+              {dropdownVisible && (
+                <div className="absolute right-0 mt-2 w-24 md:w-48 bg-white rounded-md shadow-lg py-1 z-50">
+                  <Link
+                    href="/"
+                    className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+                  >
+                    Profile
+                  </Link>
+                  <div
+                    className="block px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : (
+            <Link href="/login" className="font-normal sm:font-medium">
+              Login
+            </Link>
+          )}
         </div>
       </div>
     </div>
